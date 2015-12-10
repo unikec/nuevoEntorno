a6f3500e2ffea399df5cecef03852cc84aae9b5b
Administrador <Administrador@WIN-L93IRKH2F9P>
2015-12-10 13:58:31 +0100

    dos

--- PrimerProyecCompartido/src/Principal.java
+++ PrimerProyecCompartido/src/Principal.java
@@ -2,5 +2,10 @@
 public class Principal {
  public static void main(String[]arg){
 	 System.out.println("hola");
+	 
+	 System.out.println(" adios");
+	 
+	 int result=3+2;
+	 System.out.println( result);
  }
 }
