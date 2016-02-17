2c0b79a37096e6bac1ff8f155aff6776cb5cafe7
unikec <garcascris@gmail.com>
2016-02-17 12:26:53 +0100

    Update OtraPrueba.java

--- nuevoEntornoFebrero/src/nuevoEntornoFebrero/OtraPrueba.java
+++ nuevoEntornoFebrero/src/nuevoEntornoFebrero/OtraPrueba.java
@@ -1,7 +1,7 @@
 package nuevoEntornoFebrero;
 
 public class OtraPrueba {
-
+// enanoooooos al poder
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("a ver si consigo que salga");
