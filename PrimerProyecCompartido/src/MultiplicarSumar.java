f980d5d80dc8ceb8790b7f11da111d2e951621b9
Administrador <Administrador@usuario-pc-cris>
2016-02-17 12:07:36 +0100

    primer comit

---
+++ PrimerProyecCompartido/src/MultiplicarSumar.java
@@ -0,0 +1,32 @@
+
+public class MultiplicarSumar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				static int producto = 1;
+				int suma =0; 
+		
+		for (int i=1;i<=10;i++)
+		{
+		suma= suma + i;
+		producto = producto* i;
+		}
+		
+		
+			
+			public static int Producto(int i) {
+			producto = producto * i;
+			return producto;
+			}
+			
+		System.out.println("valor de la suma "+suma);
+		System.out.println("valor del producto "+producto);
+		
+		
+	}//END CLASS
+	
+	
+
+	//end method
+
+}
---
+++ PrimerProyecCompartido/src/MultiplicarSumar2.java
@@ -0,0 +1,19 @@
+
+public class MultiplicarSumar2 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		int suma = 0, producto = 1;
+
+		for (int i = 1; i <= 10; i++) {
+			suma = suma + i;
+			producto = producto * i;
+
+		}
+		System.out.println("valor de la suma " + suma);
+		System.out.println("valor del producto " + producto);
+
+	}
+
+}
